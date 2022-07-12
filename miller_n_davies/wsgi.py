8d07042fa6c44b545e2636619ea6f42725b395ce
Odden69 <helena.joh2@gmail.com>
2022-07-12 08:48:39 +0000

    setup: Start django project, closes #37

---
+++ miller_n_davies/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for miller_n_davies project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miller_n_davies.settings')
+
+application = get_wsgi_application()
