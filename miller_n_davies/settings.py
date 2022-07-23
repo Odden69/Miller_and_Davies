183be32129183d164622088c712c19f3a2fecd6b
Odden69 <helena.joh2@gmail.com>
2022-07-23 06:24:15 +0000

    setup: Do third commit for deployment
    Remove the secret key and set debug

--- miller_n_davies/settings.py
+++ miller_n_davies/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7+zw+2+f@)nk&niksasb-tl03cdila-)8g#$23d%@r*%1p(u36'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['hj-miller-and-davies.herokuapp.com', 'localhost']
 
