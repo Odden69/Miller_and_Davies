f17caeeb79aeac9f51658d67bb9b7f827226cf8e
Odden69 <helena.joh2@gmail.com>
2022-10-14 14:37:02 +0000

    todo: Add response messages to basket view

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from products.models import Product
 
 
 def basket(request):
@@ -15,11 +17,16 @@ def add_to_basket(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
+    product = get_object_or_404(Product, pk=item_id)
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
+        messages.success(
+            request, f'Updated the quantity of \
+            {product.name}s to {basket[item_id]}')
     else:
         basket[item_id] = quantity
+        messages.success(request, f'{product.name} was added to your basket')
 
     request.session['basket'] = basket
 
@@ -34,8 +41,11 @@ def adjust_quantity_in_basket(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
+    product = get_object_or_404(Product, pk=item_id)
 
     basket[item_id] = quantity
+    messages.success(request, f'Updated {product.name} quantity to \
+        {basket[item_id]}')
     request.session['basket'] = basket
 
     return redirect(redirect_url)
@@ -47,8 +57,16 @@ def remove_from_basket(request, item_id):
     """
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
+    product = get_object_or_404(Product, pk=item_id)
 
-    basket.pop(item_id)
-    request.session['basket'] = basket
+    try:
+        basket.pop(item_id)
+        messages.success(
+            request, f'{product.name} was deleted from your basket')
 
-    return redirect(redirect_url)
+        request.session['basket'] = basket
+        return redirect(redirect_url)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item {e}')
+        return redirect(redirect_url)
