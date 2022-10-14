657349e6542466ad87c2d5542a73beecc9673142
Odden69 <helena.joh2@gmail.com>
2022-10-14 14:38:56 +0000

    todo: Add messages to favorite view, closes #51

--- favorites/views.py
+++ favorites/views.py
@@ -1,18 +1,26 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
+from django.contrib import messages
+from products.models import Product
 
 
 def adjust_favorites(request, product_id):
     """
     Add or delete a product in the users favorite list
+
     """
-    product = int(product_id)
+    product_id = int(product_id)
     favorites = request.session.get('favorites', [])
     redirect_url = request.POST.get('redirect_url')
+    product = get_object_or_404(Product, pk=product_id)
 
-    if product in favorites:
-        favorites.remove(product)
+    if product_id in favorites:
+        favorites.remove(product_id)
+        messages.info(
+            request, f'{product.name} was removed from your favorites')
     else:
-        favorites.append(product)
+        favorites.append(product_id)
+        messages.info(
+            request, f'{product.name} was added to your favorites')
 
     request.session['favorites'] = favorites
 
