a8bbeabae2ce419ea7e88f5c2b646dba33188b29
Odden69 <helena.joh2@gmail.com>
2022-11-05 06:54:10 +0000

    setup: Create the checkout app, closes #52

---
+++ checkout/signals.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+"""
+This code is copied in its whole from the CI boutique ado project
+"""
+
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total on lineitem update/create
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_delete(sender, instance, **kwargs):
+    """
+    Update order total on lineitem delete
+    """
+    instance.order.update_total()
--- miller_n_davies/settings.py
+++ miller_n_davies/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'products',
     'basket',
     'favorites',
+    'checkout',
 
     # other
     'storages',
