6c964d68cfb4d946211bd72902d5849247ddf50b
Odden69 <helena.joh2@gmail.com>
2022-11-13 21:35:46 +0000

    setup: Add users names to sign up, closes #54

--- miller_n_davies/settings.py
+++ miller_n_davies/settings.py
@@ -125,6 +125,7 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
+ACCOUNT_FORMS = {'signup': 'miller_n_davies.forms.CustomSignupForm'}
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
