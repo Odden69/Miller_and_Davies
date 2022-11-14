c3b18222585bdecbc5a94b998324eeeb64eaa645
Odden69 <helena.joh2@gmail.com>
2022-11-14 17:46:27 +0000

    feat: Add order history to prof. page, closes #23

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('profile', views.profile, name='profile'),
+    path('profile/order_history/<order_number>',
+         views.order_history, name='order_history'),
 ]
