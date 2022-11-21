406c8d97726abd2daeb55d2f03a7047729d8403b
Odden69 <helena.joh2@gmail.com>
2022-11-21 16:58:32 +0000

    fix: Remove extra code in settings.py

--- miller_n_davies/settings.py
+++ miller_n_davies/settings.py
@@ -117,11 +117,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-
-# Ta bort den här innan produktion
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -239,9 +234,8 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'millerndavies@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS =True
+    EMAIL_USE_TLS = True
     EMAIL_PORT = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
